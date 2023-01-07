bb994f51b47298b6b493aa094534167ec6fb8526
alexs41 <alexkaygorodov@gmail.com>
2023-01-08 00:24:29 +0600

    Сверстал NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,15 +5,18 @@ export default function NavBar(props) {
     const {} = props;
     return (
         <>
-            <div className="nav-bar">
-                <button className='close-button'></button>
-                <ul className="nav-bar-menu">
-                    <li className="nav-bar-menu__item">Главная</li>
-                    <li className="nav-bar-menu__item">Фильмы</li>
-                    <li className="nav-bar-menu__item">Сохранённые фильмы</li>
-                </ul>
-                <button className="button account-button" type="button">Аккаунт<img src={accountButtonIcon} className="account-button__icon" alt="account-icon"/></button>
+            <div className="popup popup_opened">
+                <div className="nav-bar">
+                    <button className="button close-button"></button>
+                    <ul className="nav-bar-menu">
+                        <li className="nav-bar-menu__item"><a href='#' className='link'>Главная</a></li>
+                        <li className="nav-bar-menu__item nav-bar-menu__item_underline"><a href='#' className='link'>Фильмы</a></li>
+                        <li className="nav-bar-menu__item"><a href='#' className='link'>Сохранённые фильмы</a></li>
+                    </ul>
+                    <button className="button account-button" type="button">Аккаунт<img src={accountButtonIcon} className="account-button__icon" alt="account-icon"/></button>
+                </div>
             </div>
+            
         </>
     );
   }
